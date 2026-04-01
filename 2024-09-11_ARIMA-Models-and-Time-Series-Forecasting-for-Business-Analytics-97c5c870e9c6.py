a978789f5aefdebeb36929915ef691fd1734642f
kylejones200 <kyletjones@gmail.com>
2026-04-01 10:21:30 -0500

    Add code: 2024-09-11_ARIMA-Models-and-Time-Series-Forecasting-for-Business-Analytics-97c5c870e9c6.py

---
+++ 2024-09-11_ARIMA-Models-and-Time-Series-Forecasting-for-Business-Analytics-97c5c870e9c6.py
@@ -0,0 +1,15 @@
+# Description: Short example for ARIMA Models and Time Series Forecasting for Business Analytics.
+
+
+# Load the time series data
+
+from data_io import read_csv
+from statsmodels.tsa.arima_model import ARIMA
+import pandas as pd
+
+data = read_csv('sales_data.csv', index_col='date')
+# Fit the ARIMA(1,1,1) model
+model = ARIMA(data, order=(1,1,1))
+model_fit = model.fit()
+# Make forecasts
+forecast = model_fit.forecast(steps=10)[0]
